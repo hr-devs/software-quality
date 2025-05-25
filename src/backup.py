6b823aad630c078b57ae1f02fe0e5434e921b6a9
TobiZelders <tobizelders@gmail.com>
2025-05-25 15:54:09 +0200

    Fixed OOP issues

--- src/backup.py
+++ src/backup.py
@@ -7,31 +7,31 @@ class Backup:
     backup_files = ['key.txt', 'urban_mobility.db']
     backup_folder = 'backups'
 
-    def backup():
-        if not os.path.exists(backup_folder):
-            os.makedirs(backup_folder)
+    def backup(self):
+        if not os.path.exists(self.backup_folder):
+            os.makedirs(self.backup_folder)
 
         timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
         backup_subfolder = f'backups/backup_{timestamp}'
         os.makedirs(backup_subfolder)
 
-        for file in backup_files:
+        for file in self.backup_files:
             if os.path.isfile(file):
                 copy(file, backup_subfolder)
-                print(f"Copied '{file}' to '{backup_folder}'.")
+                print(f"Copied '{file}' to '{self.backup_folder}'.")
             else:
                 print(f"'{file}' is not a valid file.")
 
         return backup_subfolder
 
-    def restore(backup_subfolder):
+    def restore(self, backup_subfolder):
         path = f'backups/{backup_subfolder}'
 
         if not os.path.exists(path) or not os.path.isdir(path):
             print(f"Backup folder '{backup_subfolder}' not found!")
             return
 
-        delete_existing_files(backup_files, '')
+        Backup.delete_existing_files(self.backup_files, '')
 
         for file_name in os.listdir(path):
             file_path = os.path.join(path, file_name)
@@ -50,13 +50,13 @@ class Backup:
             else:
                 print(f"File not found: {file_path}")
 
-    def get_backup_list():
+    def get_backup_list(self):
         backup_list = []
-        if not os.path.exists(backup_folder) or not os.path.isdir(backup_folder):
+        if not os.path.exists(self.backup_folder) or not os.path.isdir(self.backup_folder):
             print(f"Backup folder not found!")
             return backup_list
 
-        for folder in os.listdir(backup_folder):
+        for folder in os.listdir(self.backup_folder):
             backup_list.append(folder)
 
         return backup_list
