7c3cb7c08790f1b950592ad8d5a4d3cabe36b823
TobiZelders <tobizelders@gmail.com>
2025-06-04 21:06:09 +0200

    Added menus for users

---
+++ src/menus/service_engineer_menu.py
@@ -0,0 +1,16 @@
+# submenu2.py
+
+from menus.base_menu import BaseMenu
+
+class ServiceEngineerMenu:
+    def display(self):
+        menu = BaseMenu("Service Engineer Menu", {
+            "1": ("Update your password", self.action),
+            "2": ("Search Scooter", self.action),
+            "3": ("Update Scooter attributes", self.action),
+            "0": ("Back", lambda: "back")
+        })
+        menu.display()
+
+    def action(self):
+        print("You selected Submenu 2 action.")
---
+++ src/menus/system_admin_menu.py
@@ -0,0 +1,28 @@
+# submenu2.py
+
+from menus.base_menu import BaseMenu
+
+class SystemAdministratorMenu:
+    def display(self):
+        menu = BaseMenu("System Administrator Menu", {
+            "1": ("Update your password", self.action),
+            "2": ("Search Scooter", self.action),
+            "3": ("Update Scooter attributes", self.action),
+            "4": ("Check list of users + roles", self.action),
+            "5": ("Add new Service Engineer", self.action),
+            "6": ("Update Service Engineer", self.action),
+            "7": ("Reset password of Service Engineer", self.action),
+            "8": ("See logs", self.action),
+            "9": ("Add new Traveller", self.action),
+            "10": ("Update Traveller", self.action),
+            "11": ("Delete Traveller", self.action),
+            "12": ("Add new Scooter", self.action),
+            "13": ("Update Scooter", self.action),
+            "14": ("Delete Scooter", self.action),
+            "15": ("Search Traveller", self.action),
+            "0": ("Back", lambda: "back")
+        })
+        menu.display()
+
+    def action(self):
+        print("You selected Submenu 2 action.")
