dad6f01e1decf73f77aea2ad53ed37162fc73d07
programmmer-dani <dani.jong01@gmail.com>
2025-06-09 22:41:32 +0200

    comment added

--- src/login.py
+++ src/login.py
@@ -1,6 +1,7 @@
 from input_handler import UserInput
 
 class Login:
+    #TODO: send password and username to database (encrypted?)
     @staticmethod
     def get_username_and_password():
         print(UserInput.get_data_input("Username: ", "Username"))
